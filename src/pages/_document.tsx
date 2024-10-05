235714ed2ca9f073ec287f8985ffc183cb62f704
Jônatas Souza Soares <56240097+jonatasJS@users.noreply.github.com>
2024-10-05 05:15:47 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+      <meta name="google-adsense-account" content="ca-pub-5593915309329672" />
+					<script
+						async
+						src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5593915309329672`}
+						crossOrigin='anonymous'
+					/>
+      </Head>
       <body>
         <Main />
         <NextScript />
